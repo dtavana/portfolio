1c9f5d0c7058b48104683821f71c37608c2ff179
dtavana <datavana@gmail.com>
2025-01-19 19:58:26 -0500

    Initial commit for Remix project with shadcn and Navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import { Link } from "@remix-run/react"
+import type { RemixLinkProps } from "@remix-run/react/dist/components";
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+} from "~/components/ui/navigation-menu";
+
+
+const baseNavigation = [
+    { name: "About", href: "/about" },
+    { name: "Projects", href: "/projects" },
+    { name: "Resume", href: "https://drive.google.com/file/d/1mtDfg42ipZmoid2htrKIir9CO6pmvIEJ/view?usp=drive_link" },
+]
+
+export function Navbar() {
+    return (
+        <NavigationMenu className="mx-auto my-4 min-w-full">
+            <NavigationMenuList>
+                <NavigationMenuItem className="font-bold mr-2"> dtavana</NavigationMenuItem>
+
+                {baseNavigation.map((item) => (
+                    <NavigationMenuItem key={item.name}>
+                        <Link to={item.href}>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                {item.name}
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                ))}
+            </NavigationMenuList>
+        </NavigationMenu>
+    );
+}
--- app/root.tsx
+++ app/root.tsx
@@ -7,9 +7,12 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+import { Navbar } from "~/components/Navbar";
+
+import styles from "./tailwind.css?url";
 
 export const links: LinksFunction = () => [
+
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
@@ -20,6 +23,7 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "stylesheet", href: styles },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -32,6 +36,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <Navbar />
         {children}
         <ScrollRestoration />
         <Scripts />
